03c59cb568136a809d6d3b65fc0180fad6e7e2fc
aliboy08 <alistair.ponce@gmail.com>
2018-02-11 01:47:07 +0800

    updated

--- ff-load-more.js
+++ ff-load-more.js
@@ -13,6 +13,7 @@
 			load_more_btn_text: 'Load more',
 			load_more_btn_class: 'btn',
 			load_more_btn_wrapper: '',
+			lazyload: true,
 		}, options );
 		
 		if( $this.data('load-on-init') ) {
@@ -23,11 +24,11 @@
 			settings.load_more = parseInt($this.data('load-more'));
 		}
 		
-		var i,
-			load_more_btn = '',
+		var load_more_btn = '',
 			total_items,
-			current_index = 0,
-			target;
+			current_index = settings.load_on_init,
+			target,
+			temp_loop_limit;
 		
 		if( settings.target ) {
 			target = $this.find(settings.target);
@@ -37,19 +38,27 @@
 		
 		total_items = target.length;
 		
-		// hide items initially
-		target.hide();
+		// hide items
+		target.each(function(index){
+			if( index >= settings.load_on_init) {
+				var this_item = $(this);
+				this_item.hide();
+				
+				if( settings.lazyload ) {
+					var images = this_item.find('img');
+					if( images.length ){
+						// Have images, prevent load
+						prevent_images_load(images);
+					}
+				}
+				
+			}
+		});
 		
-		// show initial items
-		for ( i = current_index; i < settings.load_on_init; i++ ) {
-			$(target[i]).fadeIn();
-		}
-		current_index = i;
-
-		if( i < total_items ) {
+		if( current_index < total_items ) {
 			
+			// add load more button
 			load_more_btn = $('<a href="#" class="'+ settings.load_more_btn_class +'">'+ settings.load_more_btn_text +'</a>');
-			
 			$this.append(load_more_btn);
 			
 			// button wrapper
@@ -60,18 +69,100 @@
 			// show more items
 			load_more_btn.click(function(e){
 				e.preventDefault();
-				for ( i = current_index; i < current_index + settings.load_more; i++ ) {
-					$(target[i]).fadeIn();
+				
+				temp_loop_limit = current_index + settings.load_more;
+				
+				if( settings.lazyload ) {
+					
+					// Lazyload
+					loopLazyLoadItems();
+					
+				} else {
+					
+					// No lazyload
+					for ( var i = current_index; i < temp_loop_limit; i++ ) {
+						$(target[i]).fadeIn();
+						current_index++;
+					}
+						
 				}
-				current_index = i;
 				
 				// no more items
-				if( i >= total_items ) {
+				if( current_index >= total_items ) {
 					// remove load more button
 					load_more_btn.remove();
 				}
+				
 			});
 			
+		}
+		
+		document.onreadystatechange = function(){
+			if( document.readyState === 'complete' ) {
+				// Document load complete
+				// Load prevented images
+				$(target).find('img[src=""]').each(function(){
+					var this_img = $(this);
+					var data_src = this_img.data('src');
+					this_img.attr('src', data_src);
+				})
+			}
+		}
+		
+		var loopItemCounter = 0;
+		var loopLazyLoadItems = function(index) {
+			
+			loopItemCounter = current_index;
+			var images_count = 0;
+			
+			var child_images = $(target[loopItemCounter]).find('img');
+			if(child_images.length){
+				// have images
+				images_count = child_images.length;
+				child_images.each(function(){
+					var img_el = $(this);
+					var img_el_src = img_el.data('src');
+					var img = new Image();
+					img.src = img_el_src;
+					img.onload = function() {
+						// single image load complete
+						img_el.attr('src', img_el_src);
+						images_count--;
+					}
+				});
+				
+				// Watch images loaded
+				var timer = setInterval(function(){
+					if( images_count == 0 ) {
+						// All images loaded on current item
+						clearInterval(timer); // stop watch
+						checkRecursiveFunction(loopItemCounter, temp_loop_limit - 1, loopLazyLoadItems);
+					} 
+				}, 100 );
+				
+			} else {
+				// no images
+				checkRecursiveFunction(loopItemCounter, temp_loop_limit - 1, loopLazyLoadItems);
+			}
+		}
+		
+		function checkRecursiveFunction(index, limit, callback){
+			$(target[index]).fadeIn();
+			current_index++;
+			if(index < limit) {
+				callback();
+			} else {
+				return;
+			}
+		}
+		
+		function prevent_images_load(images){
+			// Dont load images
+			images.each(function(){
+				var img_src = $(this).attr('src');
+				$(this).attr('data-src', img_src);
+				$(this).attr('src', '');
+			});
 		}
 		
 		return this;
